7b6ae41fa36e28f3868de799e8f0fe374a7f0700
Ziteng Zetten Wang <mymisting@gmail.com>
2021-03-02 05:00:21 +0000

    previous week

--- draft/DraftSort.hs
+++ draft/DraftSort.hs
@@ -13,37 +13,61 @@ import Text.Printf (printf)
 import qualified Test.ChasingBottoms as CB
 import qualified Test.QuickCheck as QC
 
-import Data.List (sortOn, (\\))
+import Test.SmallCheck
+import Test.SmallCheck.Series
+import Data.List (sortOn, (\\), maximumBy, minimumBy)
 import qualified Data.Map as Map
 import Data.Bifunctor
+import Debug.Trace
+import Control.Lens
+import Data.Ord
 
-type ConstructorName = String
-type TypeName = String
-data DataAnalysis = Instance TypeName ConstructorName [DataAnalysis] deriving (Show, Eq)
+data DataAnalysis =
+  Instance  { typeName          :: String
+            , constructorName   :: String
+            , parameters        :: [DataAnalysis]
+            , height            :: Int
+            } deriving (Show, Eq)
+
+analyzeWithHeight :: (Analyze a) => a -> ([DataAnalysis], Int)
+analyzeWithHeight a =
+    let instances = [analyze a] in
+        (instances, 1 + (maximum $ map height instances))
+
+analyzeWithHeight2 :: (Analyze a, Analyze b) => a -> b -> ([DataAnalysis], Int)
+analyzeWithHeight2 a b =
+    let instances = [analyze a, analyze b] in
+        (instances, 1 + (maximum $ map height instances))
 
 class Analyze a where analyze :: a -> DataAnalysis
-instance Analyze Int where analyze x = Instance "Int" (if x == 0 then "Z" else if x > 0 then "P" else "N") []
-instance Analyze Bool where analyze x = Instance "Bool" (show x) []
-instance Analyze Char where analyze x = Instance "Char" "_" []
+instance Analyze Int where analyze x = Instance "Int" (if x == 0 then "Z" else if x > 0 then "P" else "N") [] 0
+instance Analyze Bool where analyze x = Instance "Bool" (show x) [] 0
+instance Analyze Char where analyze x = Instance "Char" "_" [] 0
 instance Analyze a => Analyze [a] where
-    analyze = \case [] -> Instance "List" "Nil" []; x:xs -> Instance "List" "Cons" [analyze x, analyze xs]
+    analyze = \case [] -> Instance "List" "Nil" [] 0; x:xs -> let (p, h) = analyzeWithHeight2 x xs in Instance "List" "Cons" p h
 instance Analyze a => Analyze (Maybe a) where
-    analyze = \case Nothing -> Instance "Maybe" "Nothing" []; Just x -> Instance "Maybe" "Just" [analyze x]
+    analyze = \case Nothing -> Instance "Maybe" "Nothing" [] 0; Just x -> let (p, h) = analyzeWithHeight x in Instance "Maybe" "Just" p h
 instance (Analyze a, Analyze b) => Analyze (Either a b) where
-    analyze = \case Left x -> Instance "Either" "Left" [analyze x]; Right x -> Instance "Either" "Right" [analyze x]
+    analyze = \case Left x -> let (p, h) = analyzeWithHeight x in Instance "Either" "Left" p h; Right x -> let (p, h) = analyzeWithHeight [x] in Instance "Either" "Right" p h
+instance (Analyze a, Analyze b) => Analyze (a, b) where
+    analyze (l, r) = let (p, h) = analyzeWithHeight2 l r in Instance "(,)" "," p h
+
+elementwiseSum :: Num a => [(a, a)] -> (a, a)
+elementwiseSum xs = bimap sum sum $ unzip xs
 
 sort :: (Analyze a, Show a, Eq a) => [a] -> Int -> [a]
 sort [] _ = []
-sort (x:xs) depth = fst $ foldl f ([], xs) [0..depth]
-  where
-    xd = analyze x
-    f (r, s) d = 
-                  let s'  = map (\x' -> (compareData xd (analyze x') d True, x')) s
-                      p   = map head $ groupOn fst s'
-                      p'  = map snd $ sortOn fst p
-                      r'  = p' ++ r
-                      s'' = s \\ p'
-                  in (r', s'')
+sort xs depth = view _1 $ foldl g (traceShowId [minimumBy (comparing (height . analyze)) xs], xs) [0..depth]
+  where    
+    g :: (Analyze a, Show a, Eq a) => ([a], [a]) -> Int -> ([a], [a])
+    g (r, s) d = 
+                  let s'  = map (\x' -> (map (\x -> compareData (analyze x) (analyze x') d True) r, x')) s
+                      s'' = (map (first elementwiseSum) $ filter (not . any isNoDiff . fst) s')
+                      p   = map last $ groupOn fst s''
+                      p'  = traceShowId (map snd $ sortOn fst p)
+                  in  (r ++ p', s \\ p')
+                      
+
 
     groupOn :: (Ord b) => (a -> b) -> [a] -> [[a]]
     groupOn f =
@@ -53,9 +77,12 @@ sort (x:xs) depth = fst $ foldl f ([], xs) [0..depth]
             Just as -> Map.insert (f a) (a:as) m
       in unpack . foldl fld Map.empty
 
+    isNoDiff :: (Int, Int) -> Bool
+    isNoDiff = (==) (0, 0)
+
 
 compareData :: DataAnalysis -> DataAnalysis -> Int -> Bool -> (Int, Int)
-compareData (Instance xt xc xps) (Instance _ yc yps) depth isRecType = case depth of
+compareData (Instance xt xc xps _) (Instance _ yc yps _) depth isRecType = case depth of
         0 -> createResult (if xc == yc then 0 else 1) isRecType
         n -> if xc /= yc
             then createResult 1 isRecType
@@ -63,10 +90,13 @@ compareData (Instance xt xc xps) (Instance _ yc yps) depth isRecType = case dept
             
     where
         createResult r isRecType = if isRecType then (r, 0) else (0, r)
-        testRecType parentTypeName (Instance t _ _) = t == parentTypeName
-        elementwiseSum xs = bimap sum sum $ unzip xs
+        testRecType parentTypeName (Instance t _ _ _) = t == parentTypeName
 
-l1 = analyze ((Left $ Left 0) :: Either (Either Int Int) Int)
-l2 = analyze ((Left $ Right 7) :: Either (Either Int Int) Int)
+main = do
+    qcLists <- QC.generate (QC.arbitrary) :: IO [(Bool, Int)]
+    -- scLists <- return $ list 5 series :: IO [Either Bool Int]
 
-test x = compareData l1 l2 x True
+    r <- evaluate $ force $ sort qcLists 5
+
+    putStrLn (printf "Input: \t%s" (show qcLists))
+    putStrLn (printf "Result: %s" (show r))
