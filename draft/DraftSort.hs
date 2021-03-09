d2248ea6db7a7d091e8c98bfac52459847c7f72e
Ziteng Zetten Wang <mymisting@gmail.com>
2021-03-09 02:25:10 +0000

    Add TED

--- draft/DraftSort.hs
+++ draft/DraftSort.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, DeriveDataTypeable, LambdaCase #-}
-module Sort where
+{-# LANGUAGE FlexibleInstances, MultiParamTypeClasses, DeriveDataTypeable, LambdaCase, NamedFieldPuns #-}
+module DraftSort where
 
 import Control.DeepSeq (force)
 import Control.Exception (evaluate)
@@ -15,12 +15,15 @@ import qualified Test.QuickCheck as QC
 
 import Test.SmallCheck
 import Test.SmallCheck.Series
-import Data.List (sortOn, (\\), maximumBy, minimumBy)
+import Data.List (sortOn, sortBy, (\\), maximumBy, minimumBy)
 import qualified Data.Map as Map
 import Data.Bifunctor
 import Debug.Trace
 import Control.Lens
 import Data.Ord
+
+import TED.Data.Tree.Diff (n, treeDist, simpleTreeDist)
+import TED.Data.Tree (Tree(..))
 
 data DataAnalysis =
   Instance  { typeName          :: String
@@ -29,28 +32,89 @@ data DataAnalysis =
             , height            :: Int
             } deriving (Show, Eq)
 
+convertDataAnalysis :: DataAnalysis -> Tree String
+convertDataAnalysis Instance {constructorName, parameters} = n constructorName (map convertDataAnalysis parameters)
+
 analyzeWithHeight :: (Analyze a) => a -> ([DataAnalysis], Int)
 analyzeWithHeight a =
     let instances = [analyze a] in
-        (instances, 1 + (maximum $ map height instances))
+        (instances, 1 + maximum (map height instances))
 
 analyzeWithHeight2 :: (Analyze a, Analyze b) => a -> b -> ([DataAnalysis], Int)
 analyzeWithHeight2 a b =
     let instances = [analyze a, analyze b] in
-        (instances, 1 + (maximum $ map height instances))
+        (instances, 1 + maximum (map height instances))
 
-class Analyze a where analyze :: a -> DataAnalysis
-instance Analyze Int where analyze x = Instance "Int" (if x == 0 then "Z" else if x > 0 then "P" else "N") [] 0
-instance Analyze Bool where analyze x = Instance "Bool" (show x) [] 0
-instance Analyze Char where analyze x = Instance "Char" "_" [] 0
+class    Analyze a    where analyze :: a -> DataAnalysis
+instance Analyze Int  where analyze x = Instance "Int"  (show $ x `compare` 0)  [] 0
+instance Analyze Bool where analyze x = Instance "Bool" (show x)                [] 0
+instance Analyze Char where analyze x = Instance "Char" "_"                     [] 0
+
 instance Analyze a => Analyze [a] where
-    analyze = \case [] -> Instance "List" "Nil" [] 0; x:xs -> let (p, h) = analyzeWithHeight2 x xs in Instance "List" "Cons" p h
+  analyze []        = Instance "List" "Nil"  [] 0
+  analyze (x:xs)    = Instance "List" "Cons" p  h
+    where (p, h)    = analyzeWithHeight2 x xs
+
 instance Analyze a => Analyze (Maybe a) where
-    analyze = \case Nothing -> Instance "Maybe" "Nothing" [] 0; Just x -> let (p, h) = analyzeWithHeight x in Instance "Maybe" "Just" p h
-instance (Analyze a, Analyze b) => Analyze (Either a b) where
-    analyze = \case Left x -> let (p, h) = analyzeWithHeight x in Instance "Either" "Left" p h; Right x -> let (p, h) = analyzeWithHeight [x] in Instance "Either" "Right" p h
-instance (Analyze a, Analyze b) => Analyze (a, b) where
-    analyze (l, r) = let (p, h) = analyzeWithHeight2 l r in Instance "(,)" "," p h
+  analyze Nothing   = Instance "Maybe" "Nothing" [] 0
+  analyze (Just x)  = Instance "Maybe" "Just" p h
+    where (p, h)    = analyzeWithHeight x
+
+instance (Analyze a, Analyze b) => Analyze (Either a b) where analyze = \case Left x -> let (p, h) = analyzeWithHeight x in Instance "Either" "Left" p h; Right x -> let (p, h) = analyzeWithHeight [x] in Instance "Either" "Right" p h
+instance (Analyze a, Analyze b) => Analyze (a, b) where analyze (l, r) = let (p, h) = analyzeWithHeight2 l r in Instance "(,)" "," p h
+
+-- | baseline: select start element x, then sort w.r.t. x
+sortWithTreeDist1 :: (Analyze a, Show a, Eq a) => [a] -> [(a, Int)]
+sortWithTreeDist1 xs =
+    let
+      xs' = map (\x -> (x, analyze x)) xs
+      s   = minimumBy (comparing (height . snd)) xs'
+      r   = map (second (simpleTreeDist (convertDataAnalysis $ snd s) . convertDataAnalysis)) xs'
+    in
+      second (const 0) s : sortOn (Down . snd) r
+
+-- | variant-2: select start x, then select the next element iteratively (s.t. maximize the distance sum)
+sortWithTreeDist2 :: (Analyze a, Show a, Eq a) => [a] -> [(a, Int)]
+sortWithTreeDist2 xs =
+    let
+      r = [(minimumBy (comparing (height . analyze)) xs, 0)]
+    in
+      view _1 $ foldl step (r, xs) [0..length xs - 1]
+  where
+    step :: (Analyze a, Show a, Eq a) => ([(a, Int)], [(a)]) -> Int -> ([(a, Int)], [a])
+    step (r, s) _ =
+      let
+        s'  = map (\x -> (x, map (compareTwo x . fst) r)) s
+        s'' = map (second sum) s'
+        p   = maximumBy (comparing snd) s''
+      in
+        (r ++ [p], s \\ [fst p])
+
+    compareTwo :: Analyze a => a -> a -> Int
+    compareTwo a b = simpleTreeDist (convertDataAnalysis $ analyze a) (convertDataAnalysis $ analyze b)
+
+-- | variant-3: select start x, then select the next element iteratively (s.t. maximize the minimum distance)
+sortWithTreeDist3 :: (Analyze a, Show a, Eq a) => [a] -> [(a, Int)]
+sortWithTreeDist3 xs =
+    let
+      r = [(minimumBy (comparing (height . analyze)) xs, 0)]
+    in
+      view _1 $ foldl step (r, xs) [0..length xs - 1]
+  where
+    step :: (Analyze a, Show a, Eq a) => ([(a, Int)], [(a)]) -> Int -> ([(a, Int)], [a])
+    step (r, s) _ =
+      let
+        s'  = map (\x -> (x, map (compareTwo x . fst) r)) s
+        s'' = map (second minimum) s'
+        p   = maximumBy (comparing snd) s''
+      in
+        (r ++ [p], s \\ [fst p])
+
+    compareTwo :: Analyze a => a -> a -> Int
+    compareTwo a b = simpleTreeDist (convertDataAnalysis $ analyze a) (convertDataAnalysis $ analyze b)
+
+sortWithTreeDist :: (Analyze a, Show a, Eq a) => [a] -> [(a, Int)]
+sortWithTreeDist = sortWithTreeDist3
 
 elementwiseSum :: Num a => [(a, a)] -> (a, a)
 elementwiseSum xs = bimap sum sum $ unzip xs
@@ -58,15 +122,15 @@ elementwiseSum xs = bimap sum sum $ unzip xs
 sort :: (Analyze a, Show a, Eq a) => [a] -> Int -> [a]
 sort [] _ = []
 sort xs depth = view _1 $ foldl g (traceShowId [minimumBy (comparing (height . analyze)) xs], xs) [0..depth]
-  where    
+  where
     g :: (Analyze a, Show a, Eq a) => ([a], [a]) -> Int -> ([a], [a])
-    g (r, s) d = 
+    g (r, s) d =
                   let s'  = map (\x' -> (map (\x -> compareData (analyze x) (analyze x') d True) r, x')) s
                       s'' = (map (first elementwiseSum) $ filter (not . any isNoDiff . fst) s')
                       p   = map last $ groupOn fst s''
                       p'  = traceShowId (map snd $ sortOn fst p)
                   in  (r ++ p', s \\ p')
-                      
+
 
 
     groupOn :: (Ord b) => (a -> b) -> [a] -> [[a]]
@@ -87,16 +151,16 @@ compareData (Instance xt xc xps _) (Instance _ yc yps _) depth isRecType = case 
         n -> if xc /= yc
             then createResult 1 isRecType
             else elementwiseSum $ zipWith (\x y -> compareData x y (depth - 1) (testRecType xt x)) xps yps
-            
+
     where
         createResult r isRecType = if isRecType then (r, 0) else (0, r)
         testRecType parentTypeName (Instance t _ _ _) = t == parentTypeName
 
-main = do
-    qcLists <- QC.generate (QC.arbitrary) :: IO [(Bool, Int)]
+main_ = do
+    qcLists <- QC.generate QC.arbitrary :: IO [Either Bool Int]
     -- scLists <- return $ list 5 series :: IO [Either Bool Int]
 
-    r <- evaluate $ force $ sort qcLists 5
+    r <- evaluate $ force $ sortWithTreeDist qcLists
 
     putStrLn (printf "Input: \t%s" (show qcLists))
     putStrLn (printf "Result: %s" (show r))
---
+++ draft/TED/Data/Tree.hs
@@ -0,0 +1,204 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+-- | A simple rose tree with ordered siblings.
+module TED.Data.Tree where
+
+import           Control.Arrow (first, second)
+import           Data.Functor
+import           Data.List
+import           Data.Monoid
+import           Data.Vector   (Vector)
+import qualified Data.Vector   as V
+
+-- $setup
+--
+-- TODO: Useful Arbitrary instances!
+--
+-- >>> :set -XScopedTypeVariables
+-- >>> import Test.QuickCheck
+-- >>> instance Arbitrary a => Arbitrary (Forest a) where arbitrary = pure (Forest mempty)
+-- >>> instance Arbitrary a => Arbitrary (Tree a) where arbitrary = Node <$> arbitrary <*> arbitrary
+
+-- * Trees and Forests
+
+-- | A 'Tree' is a node labelled with an element of 'sigma'.
+data Tree sigma = Node
+  { treeLabel    :: sigma
+  , treeBranches :: Forest sigma
+  }
+  deriving (Ord, Eq)
+
+instance Show l => Show (Tree l) where
+  show (Node l (Forest v))
+    | V.null v = show l
+    | otherwise =
+      let atom = show l
+          rest = map show $ V.toList v
+          words = atom : rest
+      in "(" <> unwords words <> ")"
+
+treeSize :: Tree l -> Int
+treeSize (Node _ f) = 1 + forestSize f
+
+-- | A 'Forest' is an ordered collection of 'Tree's.
+newtype Forest sigma = Forest
+  { forestTrees :: Vector (Tree sigma) }
+  deriving (Ord, Eq)
+
+instance Semigroup (Forest sigma) where
+  (Forest l) <> (Forest r) = Forest (l <> r)
+
+instance Monoid (Forest sigma) where
+  mempty = Forest mempty
+
+instance Show l => Show (Forest l) where
+  show (Forest children) = "[" <> (unwords . map show . V.toList $ children) <> "]"
+
+forestSize :: Forest l -> Int
+forestSize (Forest ts) = V.foldl' (\a t -> a + treeSize t) 0 ts
+
+-- * Construction
+
+-- | Construct a one-node 'Tree'.
+singleton :: sigma -> Tree sigma
+singleton l = Node l mempty
+
+forest :: [Tree l] -> Forest l
+forest l = Forest (V.fromList l)
+
+singletonForest :: Tree l -> Forest l
+singletonForest t = Forest (V.singleton t)
+
+-- | Graft a 'Tree' into a 'Forest' on the left.
+--
+-- prop> \t f -> leftMost (graftLeft t f) == Just (t, f)
+graftLeft :: Tree sigma -> Forest sigma -> Forest sigma
+graftLeft t (Forest ts) = Forest (t `V.cons` ts)
+
+-- | Graft a 'Tree' into a 'Forest' on the right.
+--
+-- prop> \t f -> rightMost (graftRight f t) == Just (f, t)
+graftRight :: Forest sigma -> Tree sigma -> Forest sigma
+graftRight (Forest ts) t = Forest (ts `V.snoc` t)
+
+-- * Maps
+
+type Pos = [Int]
+
+mapForest :: (l -> l') -> Forest l -> Forest l'
+mapForest f = mapForestPos (\_ l -> f l)
+
+mapForestPos :: (Pos -> l -> l') -> Forest l -> Forest l'
+mapForestPos f = goF []
+  where
+    goT path (Node l fs) = Node (f path l) (goF path fs)
+    goF path (Forest ts) = Forest (V.imap (\ix a -> goT (path <> [ix]) a) ts)
+
+foldLabel :: acc -> (acc -> l -> acc) -> Forest l -> acc
+foldLabel acc f = goF acc
+  where
+    goT acc (Node l fs) = goF (f acc l) fs
+    goF acc (Forest ts) = V.foldr (flip goT) acc ts
+
+foldEdges :: acc -> (acc -> l -> l -> acc) -> Forest l -> acc
+foldEdges acc f = goF acc
+  where
+    goT acc (Node p fs@(Forest cs)) = goF (V.foldr (\(Node c _) a -> f a p c) acc cs) fs
+    goF acc (Forest ts) = V.foldl goT acc ts
+
+-- | Visit each node and edge.
+--
+-- We visit, in order: the incoming edge, the node, it's outgoing edges.
+foldTree :: acc -> (acc -> l -> acc) -> (acc -> l -> l -> acc) -> Forest l -> acc
+foldTree acc node edge = goF acc
+  where
+    goT acc (Node p fs) = acc
+    goF acc (Forest ts) = acc
+
+dot :: Show l => Forest l -> String
+dot f =
+  let fl = mapForestPos (\p l -> (show . intercalate "." . map show $ p, show $ show l)) f
+  in unlines $ [ "digraph {" ] <>
+               foldLabel [] (\acc (p, l) -> ("  " <> p <> "[label=" <> l <> "];") : acc ) fl <>
+               foldEdges [] (\acc (p, _) (c, _) -> ("  " <> p <> " -> " <> c <> ";") : acc) fl <>
+               [ "}" ]
+
+showS :: Forest l -> String
+showS = show . mapForestPos (\p _ -> intercalate "." . map show $ p)
+
+-- * Predicates and accessors
+
+-- | Check that a 'Forest' is empty.
+null :: Forest sigma -> Bool
+null (Forest trees) = V.null trees
+
+-- | The number of children a tree has.
+degree :: Tree sigma -> Int
+degree (Node _ (Forest branches)) = V.length branches
+
+-- | The number of nodes in a 'Tree'.
+treeNodes :: Tree sigma -> Int
+treeNodes (Node _ bs) = 1 + forestNodes bs
+
+-- | The number of nodes in a 'Forest'.
+forestNodes :: Forest sigma -> Int
+forestNodes (Forest ts) = V.foldl (\c t -> c + treeNodes t) 0 ts
+
+-- * Destruction
+
+uncons :: Forest l -> Maybe (l, Forest l, Forest l)
+uncons (Forest v)
+  | V.null v = Nothing
+  | otherwise =
+    let (Node l g) = V.head v
+        t = Forest $ V.tail v
+    in Just (l, g, t)
+
+unsnoc :: Forest l -> Maybe (Forest l, l, Forest l)
+unsnoc (Forest v)
+  | V.null v = Nothing
+  | otherwise =
+      let (Node l g) = V.last v
+          t = Forest $ V.init v
+      in Just (t, l, g)
+
+
+-- | Split a 'Tree' into it's root label and a 'Forest' of branches.
+--
+-- prop> \(l :: Int) -> splitTree (singleton l) == (l, mempty)
+-- prop> \(l :: Int) f -> splitTree (Node l f) == (l, f)
+splitTree :: Tree sigma -> (sigma, Forest sigma)
+splitTree (Node l b) = (l, b)
+
+-- | Get the left-most 'Tree' in a 'Forest'.
+leftMost :: Forest sigma -> Maybe (Tree sigma, Forest sigma)
+leftMost (Forest f)
+    | V.null f = Nothing
+    | otherwise =
+        let l = V.head f
+            t = V.tail f
+        in Just (l, Forest t)
+
+-- | Get the right-most 'Tree' in a 'Forest'.
+rightMost :: Forest sigma -> Maybe (Forest sigma, Tree sigma)
+rightMost (Forest f)
+    | V.null f = Nothing
+    | otherwise =
+        let r = V.last f
+            t = V.init f
+        in Just (Forest t, r)
+
+-- | Decompose a forest at the root of the left-most tree.
+--
+-- Returns the left-most tree's root label and branches, and the right trees.
+--
+-- prop> \f t -> leftRoot (graftLeft t f) == Just (splitTree t, f)
+leftRoot :: Forest sigma -> Maybe ((sigma, Forest sigma), Forest sigma)
+leftRoot f = first splitTree <$> leftMost f
+
+-- | Decompose a forest at the root of the right-most tree.
+--
+-- Returns the left trees, right-most tree's root label and branches.
+-- prop> \f t -> rightRoot (graftRight f t) == Just (f, splitTree t)
+rightRoot :: Forest sigma -> Maybe (Forest sigma, (sigma, Forest sigma))
+rightRoot f = second splitTree <$> rightMost f
+
